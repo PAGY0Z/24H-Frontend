9449ec786b6094a5dded60dc25b129263c0da0fd
PAGYOZ <pierre-alexandre.grosset@epitech.eu>
2025-05-17 15:13:20 +0400

    [ADD] : Adding new shit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import React from "react";
+
 export default function Home() {
-  return;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Welcome to the End Page Museum</h1>
+      </div>
+    </>
+  );
 }
